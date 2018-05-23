41bdbe73cb60c3d9b0b42f83b7b17a07483b3224
Lipu Fei <lipu.fei815@gmail.com>
2018-05-23 14:20:25 +0200

    Update cx_Freeze to 5.1.1
    Old version doesn't support Python 3.6

--- projects/cxfreeze.cmake
+++ projects/cxfreeze.cmake
@@ -3,9 +3,9 @@ if(BUILD_OS_OSX)
     set(_patch_command patch cx_Freeze/freezer.py ${CMAKE_SOURCE_DIR}/projects/cxfreeze_exclude_a.patch)
 endif()
 
-set(_url "https://github.com/anthony-tuininga/cx_Freeze/archive/5.0.tar.gz")
+set(_url "https://github.com/anthony-tuininga/cx_Freeze/archive/5.1.1.tar.gz")
 if(BUILD_OS_WINDOWS)
-    set(_url https://github.com/anthony-tuininga/cx_Freeze/archive/5.0.1.tar.gz)
+    set(_url https://github.com/anthony-tuininga/cx_Freeze/archive/5.1.1.tar.gz)
 endif()
 
 ExternalProject_Add(cx_Freeze
