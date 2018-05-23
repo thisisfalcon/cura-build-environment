815a08076838fd516fcc4a996c7906fffaca00e2
Lipu Fei <lipu.fei815@gmail.com>
2018-05-23 14:15:19 +0200

    Update Python to 3.6.6

--- projects/python.cmake
+++ projects/python.cmake
@@ -1,15 +1,19 @@
 set(python_patch_command "")
-set(python_configure_command ./configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-shared --with-threads --without-pymalloc)
+set(python_configure_command ./configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-shared --enable-optimizations --enable-ipv6 --with-threads --without-pymalloc)
 set(python_build_command make)
 set(python_install_command make install)
 
 if(BUILD_OS_OSX)
     # See http://bugs.python.org/issue21381
     # The interpreter crashes when MACOSX_DEPLOYMENT_TARGET=10.7 due to the increased stack size.
-    set(python_patch_command sed -i".bak" "9271,9271d" <SOURCE_DIR>/configure)
+    set(python_patch_command sed -i".bak" "9467,9467d" <SOURCE_DIR>/configure)
+    # Since OS X 10.11, System Integrity Protection (SIP) is introduced to enhance security. It clears
+    # DYLD_LIBRARY_PATH for all sub-processes so now setting DYLD_LIBRARY_PATH won't work. We now compile
+    # Python by linking to the OS X framework to avoid this problem.
+    set(python_configure_command ./configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-framework --disable-shared --enable-optimizations --enable-ipv6 --with-threads --without-pymalloc)
     # OS X 10.11 removed OpenSSL. Brew now refuses to link so we need to manually tell Python's build system
     # to use the right linker flags.
-    set(python_configure_command CPPFLAGS=-I/usr/local/opt/openssl/include LDFLAGS=-L/usr/local/opt/openssl/lib ${python_configure_command})
+    set(python_configure_command CPPFLAGS=-I/usr/local/opt/openssl/include LDFLAGS=-L/usr/local/opt/openssl/lib PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig ${python_configure_command})
 endif()
 
 if(BUILD_OS_LINUX)
@@ -33,8 +37,8 @@ if(BUILD_OS_WINDOWS)
 endif()
 
 ExternalProject_Add(Python
-    URL https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
-    URL_MD5 3fe8434643a78630c61c6464fe2e7e72
+    URL https://www.python.org/ftp/python/3.6.6/Python-3.6.6.tgz
+    URL_MD5 9a080a86e1a8d85e45eee4b1cd0a18a2
     PATCH_COMMAND ${python_patch_command}
     CONFIGURE_COMMAND "${python_configure_command}"
     BUILD_COMMAND ${python_build_command}
