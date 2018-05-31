8f80c69b13d9144c4003e982e4b417a75f972d3e
Lipu Fei <lipu.fei815@gmail.com>
2018-05-31 12:38:24 +0200

    Update env_osx.sh

--- env_osx.sh
+++ env_osx.sh
@@ -3,7 +3,7 @@
 export MACOSX_DEPLOYMENT_TARGET=10.7
 export CC=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
 export CXX=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++
-export CFLAGS="-I/usr/local/opt/openssl/include -stdlib=libc++"
-export CXXFLAGS="-I/usr/local/opt/openssl/include -stdlib=libc++"
-export LDFLAGS="-L/usr/local/opt/openssl/lib -stdlib=libc++"
-
+export CPPFLAGS="-I/usr/local/opt/openssl/include -stdlib=libc++"
+export CFLAGS="-stdlib=libc++"
+export CXXFLAGS="-stdlib=libc++"
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
